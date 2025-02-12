c9368c727c6d97bf2c57a866e8ebe813cc8012af
Glanzmann100 <72605887+Glanzmann100@users.noreply.github.com>
2025-02-11 23:59:19 -0300

    Create py.py

---
+++ py.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite o valor: "))
+acrescimo = int(input("Digite o valor do acréscimo: "))
+aumento = numero * (acrescimo / 100)
+novo_valor = numero + aumento
+print(novo_valor)
